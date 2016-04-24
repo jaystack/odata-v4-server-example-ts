bd7d83d58ff476a6eff230835eb90180591a01cc
PeterAronZentai <peter.zentai@avangrade.com>
2016-04-24 03:05:40 +0100

    x

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,10 @@
 /// <reference path="../typings/tsd.d.ts" />
 
 import * as express from 'express'
-import {MongoClient, Db} from 'mongodb'
+//import {MongoClient, Db} from 'mongodb'
 
 const app = express()
-let db:Db = null;
+// let db:Db = null;
 
 // MongoClient.connect(process.env.MONGO_URL, (err, _db) => {
 //   console.log("mongodb connected");
@@ -12,15 +12,15 @@ let db:Db = null;
 // })
 console.log("@#@#@#", process.env.MONGO_URL)
 
-app.get('/insert', (req, res) => {
-  db.collection("users").insertOne({name:'zp'})
-    .then(r => {
-      res.json(r)
-    })
-    .catch(e => {
-      res.status(500).json(e)
-    })
-})
+// app.get('/insert', (req, res) => {
+//   db.collection("users").insertOne({name:'zp'})
+//     .then(r => {
+//       res.json(r)
+//     })
+//     .catch(e => {
+//       res.status(500).json(e)
+//     })
+// })
 
 app.get("/test", (req, res) => {
   res.send("OK")
