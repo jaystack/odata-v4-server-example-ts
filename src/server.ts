075410d7348c1903ca7150554ab8d43d76074e7d
PeterAronZentai <peter.zentai@avangrade.com>
2016-04-24 03:12:15 +0100

    x

--- src/server.ts
+++ src/server.ts
@@ -1,26 +1,27 @@
 /// <reference path="../typings/tsd.d.ts" />
 
 import * as express from 'express'
-//import {MongoClient, Db} from 'mongodb'
+import {MongoClient, Db} from 'mongodb'
 
 const app = express()
-// let db:Db = null;
+let db:Db = null;
 
-// MongoClient.connect(process.env.MONGO_URL, (err, _db) => {
-//   console.log("mongodb connected");
-//   db = _db;
-// })
-//console.log("@#@#@#", process.env.MONGO_URL)
+MongoClient.connect(process.env.MONGO_URL, (err, _db) => {
+  console.log("mongodb connected");
+  db = _db;
+})
 
-// app.get('/insert', (req, res) => {
-//   db.collection("users").insertOne({name:'zp'})
-//     .then(r => {
-//       res.json(r)
-//     })
-//     .catch(e => {
-//       res.status(500).json(e)
-//     })
-// })
+console.log("@#@#@#", process.env.MONGO_URL)
+
+app.get('/insert', (req, res) => {
+  db.collection("users").insertOne({name:'zp'})
+    .then(r => {
+      res.json(r)
+    })
+    .catch(e => {
+      res.status(500).json(e)
+    })
+})
 
 app.get("/test", (req, res) => {
   res.send("OK")
